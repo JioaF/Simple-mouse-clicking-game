2fb787e5b3fe320bf2c03f6c6e32163fecb4c8be
JioaF <amuhammadsalman45@gmail.com>
2021-05-19 12:23:16 +0700

    adding small changes

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 var panel = document.getElementsByClassName("gameArea")[0];
 var startBtn = panel.getElementsByTagName("button")[0];
-
 var boxObj = document.createElement("div");
-    boxObj.style.width = "50px";
-    boxObj.style.height = "50px";
+    boxObj.style.width = "20px";
+    boxObj.style.height = "20px";
     boxObj.style.position = "absolute";
     boxObj.style.backgroundColor = "black";
     boxObj.setAttribute("id", "box");
+
 var scoreCounter = document.getElementById("score");
 var timeCounter = document.getElementById("timer");
 
 var score = 0;
-var timer = 10;
+var timer = 60;
 var intv;
-startBtn.addEventListener("click", ()=>{
+var date = new Date();
+
+startBtn.addEventListener("click", ()=>{ 
     var rand = Math.floor(Math.random() * 100) + 0;
     boxObj.style.top = `${rand}px`;
     boxObj.style.left = `${rand}px`;
@@ -24,7 +26,7 @@ startBtn.addEventListener("click", ()=>{
         timeCounter.innerHTML = `Time : ${timer -= 1}`;
         if(timer == 0){
             clearInterval(intv);
-            alert("Your Score is " + score);
+            alert(`Result :\nClicked : ${score}`);
             boxObj.style.display = "none";
         }
     }, 1000);
@@ -32,14 +34,12 @@ startBtn.addEventListener("click", ()=>{
 });
 
 
-
-
 boxObj.addEventListener("click", ()=>{
     console.log("pressed");
-    boxObj.style.top = `${ Math.floor(Math.random() * 100) + 0}px`;
-    boxObj.style.left = `${ Math.floor(Math.random() * 100) + 0}px`;
-    boxObj.style.right = `${ Math.floor(Math.random() * 100) + 0}px`;
-    boxObj.style.bottom = `${ Math.floor(Math.random() * 100) + 0}px`;
+    boxObj.style.top = `${ (Math.floor(Math.random() * 100) + 0) + date.getSeconds()}px`;
+    boxObj.style.left = `${ (Math.floor(Math.random() * 100) + 0) + date.getSeconds()}px`;
+    boxObj.style.right = `${ (Math.floor(Math.random() * 100) + 0) + date.getSeconds()}px`;
+    boxObj.style.bottom = `${ (Math.floor(Math.random() * 100) + 0) + date.getSeconds()}px`;
     scoreCounter.innerHTML = `Score : ${score+=1}`;
     console.log(`top:${boxObj.style.top}, right:${boxObj.style.right}, left:${boxObj.style.left}, bottom:${boxObj.style.bottom}`);
 });
