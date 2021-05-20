2c5bc7d17d76fb188f120622f569dac840467846
JioaF <amuhammadsalman45@gmail.com>
2021-05-20 21:18:29 +0700

    Refactoring : adding es6 coding technique

--- main.js
+++ main.js
@@ -1,45 +1,95 @@
-var panel = document.getElementsByClassName("gameArea")[0];
-var startBtn = panel.getElementsByTagName("button")[0];
-var boxObj = document.createElement("div");
-    boxObj.style.width = "20px";
-    boxObj.style.height = "20px";
-    boxObj.style.position = "absolute";
-    boxObj.style.backgroundColor = "black";
-    boxObj.setAttribute("id", "box");
+class domObj{
+    constructor(element, method){
+        switch (method) {
+            case "create":
+                this.element = document.createElement(element);
+                break;
+            case "id":
+                this.element = document.getElementById(element);
+            break;
+            case "class":
+                this.element = document.getElementsByClassName(element)[0];
+            break;
+        }
+        
+    } 
 
-var scoreCounter = document.getElementById("score");
-var timeCounter = document.getElementById("timer");
+    getElement(){
+        return this.element;
+    }
 
-var score = 0;
-var timer = 60;
-var intv;
+    setObjProps(width, height, bgcolor, position = "static", props = ""){
+        this.element.style.width = width;
+        this.element.style.height = height;
+        this.element.style.position = position;
+        this.element.style.backgroundColor = bgcolor;
+        this.element.setAttribute("id", props);
+    }
+
+}
+
+class gameManager{
+    constructor(score, time){
+        this.score = score;
+        this.time = time;
+        this.rand;
+    }
+    
+    scoreCounter(nStart,obj){
+        return obj.innerHTML = `Score : ${this.score += nStart}`;
+    }
+
+    timeCounter(ms = 0, obj1, obj2){
+        let isStop;
+        let intv = setInterval(() => {
+            obj1.innerHTML = `Time : ${this.time -= 1}`;
+            if(this.time == 0){
+                clearInterval(intv);
+                obj2.style.display = "none";
+                alert(`Result :\nClicked : ${this.score}`);
+            }
+        }, ms);
+    }
+
+    rng(nstart, nmax){
+        this.rand = Math.floor(Math.random() * nmax) + nstart;
+        console.log("rng" + this.rand);
+        return this.rand;
+    }
+
+    movepos(position = 0, obj){
+        obj.style.top =`${position}px`;
+        obj.style.right =`${position}px`;
+        obj.style.bottom =`${position}px`;
+        obj.style.left =`${position}px`; 
+    }
+
+    logging(obj){
+        let str = `top:${obj.style.top}, right:${obj.style.right}, left:${obj.style.left}, bottom:${obj.style.bottom}`;
+        return str;
+    }
+
+}
+const panel = new domObj("gameArea", "class");
+const startBtn = new domObj("start", "id");
+var box = new domObj("div", "create");
+box.setObjProps("20px", "20px", "black", "absolute", "box");
+var scoreCounter = new domObj("score", "id");
+var timer = new domObj("timer", "id");
+
+var gm = new gameManager(0, 10);
 var date = new Date();
 
-startBtn.addEventListener("click", ()=>{ 
-    var rand = Math.floor(Math.random() * 100) + 0;
-    boxObj.style.top = `${rand}px`;
-    boxObj.style.left = `${rand}px`;
-    boxObj.style.right = `${rand}px`;
-    boxObj.style.bottom = `${rand}px`;
-    scoreCounter.innerHTML = "Score : " + score;
-    intv = setInterval(() => {
-        timeCounter.innerHTML = `Time : ${timer -= 1}`;
-        if(timer == 0){
-            clearInterval(intv);
-            alert(`Result :\nClicked : ${score}`);
-            boxObj.style.display = "none";
-        }
-    }, 1000);
-    panel.append(boxObj);
+startBtn.getElement().addEventListener("click", ()=>{
+    panel.getElement().append(box.getElement());
+    gm.movepos(gm.rng(0, 100), box.getElement());
+    gm.timeCounter(1000, timer.getElement(), box.getElement());
+    gm.scoreCounter(0, scoreCounter.getElement());
 });
 
-
-boxObj.addEventListener("click", ()=>{
-    console.log("pressed");
-    boxObj.style.top = `${ (Math.floor(Math.random() * 100) + 0) + date.getSeconds()}px`;
-    boxObj.style.left = `${ (Math.floor(Math.random() * 100) + 0) + date.getSeconds()}px`;
-    boxObj.style.right = `${ (Math.floor(Math.random() * 100) + 0) + date.getSeconds()}px`;
-    boxObj.style.bottom = `${ (Math.floor(Math.random() * 100) + 0) + date.getSeconds()}px`;
-    scoreCounter.innerHTML = `Score : ${score+=1}`;
-    console.log(`top:${boxObj.style.top}, right:${boxObj.style.right}, left:${boxObj.style.left}, bottom:${boxObj.style.bottom}`);
+box.getElement().addEventListener("click", ()=>{
+    gm.scoreCounter(1, scoreCounter.getElement());
+    gm.movepos(gm.rng(0, 100), box.getElement());
+    console.log(gm.logging(box.getElement()));
 });
+
